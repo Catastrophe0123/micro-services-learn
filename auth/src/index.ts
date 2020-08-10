718c693ca9888c683195b5fc956ac338d77ec5b4
Navaneeth <vnavaneeth15@gmail.com>
2020-08-10 17:21:57 +0530

    basic ingress server setup

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,10 @@ import { json } from 'express';
 const app = express();
 app.use(json());
 
+app.get('/api/users/currentuser', (req, res) => {
+	res.send('hello world boi ');
+});
+
 app.listen(3000, () => {
-	console.log('listening on 3000');
+	console.log('listening on 3000!');
 });
